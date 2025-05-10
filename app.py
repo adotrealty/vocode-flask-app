88cf5166b5066672a27420c62c4535a257faf60f
adotrealty <li@adot.com.au>
2025-05-10 20:49:36 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, Response
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,9 +9,12 @@ def home():
 
 @app.route("/voice", methods=["POST"])
 def voice():
-    return "<?xml version='1.0' encoding='UTF-8'?><Response><Say>Hello from Vocode Flask on Railway!</Say></Response>", 200, {"Content-Type": "application/xml"}
-
-import os
+    print("📞 Incoming call from Twilio")
+    twiml_response = """<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Say>Hello! This is your Railway app speaking. It works!</Say>
+</Response>"""
+    return Response(twiml_response, mimetype="text/xml")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
